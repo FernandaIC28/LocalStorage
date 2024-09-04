04d101ac9e0353a6c4bef6452fbf1ed0e4be7b94
fernandainzaurralde <fer.inza1996@gmail.com>
2024-09-04 17:21:19 -0300

    Update index.js

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.getElementById ("buttonText").addEventListener("click"),fuction(){
+var inputText = document.getElementById("inputText").value;
+localStorage.setItem("datoGuardado",inputText);
+  alert("Dato guardado en LocalStorage);
+        )}
+        
